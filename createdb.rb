d40117b34b4f470034aae58b8cae99590351b022
Sinthuja Nagalingam <sinthu.n@gmail.com>
2020-03-18 12:50:39 -0500

    added domain model to app

--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,41 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :college do
   primary_key :id
-  String :title
-  String :description, text: true
-  String :date
-  String :location
+  String :name
+  String :city
+  String :state
+  Integer :tuition_fees
+  Integer :room_meals
 end
-DB.create_table! :rsvps do
+DB.create_table! :cost do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :college_id
+  foreign_key :user_id
+  Integer :grants
+  Integer :loans
+  Integer :work_study
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+events_table = DB.from(:college)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(name: "University of Illinois at Urbana-Champaign",
+                    city: "Champaign",
+                    state: "IL",
+                    tuition_fees: "21870",
+                    room_meals: "12252")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(name: "DePaul University",
+                    city: "Chicago",
+                    state: "IL",
+                    tuition_fees: "42468",
+                    room_meals: "15093")
+puts "Success!"
